059069759508d37a09d86810f805c500c6e47909
Gustavo Macedo <91705195+gustavomotamacedo@users.noreply.github.com>
2026-01-30 02:45:27 -0300

    atualiza cors

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 # Configurar CORS para permitir requisições do frontend
 CORS(app, resources={
     r"/api/*": {
-        "origins": ["http://localhost:3000", "http://127.0.0.1:3000", "https://portfolio-frontend-green-zeta.vercel.app"],
+        "origins": ["http://localhost:3000", "http://127.0.0.1:3000", "https://portfolio-frontend-green-zeta.vercel.app", "https://portfolio-backend-iota-rose.vercel.app"],
         "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
         "allow_headers": ["Content-Type", "Authorization"],
         "supports_credentials": True
